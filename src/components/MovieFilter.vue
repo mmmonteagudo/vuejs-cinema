1d8e1191acfc365b204eb541ebd723a2519598b0
Marvin M. Monteagudo <marvin.monteagudo@gmail.com>
2020-08-28 12:55:49 +0800

    update

--- src/components/MovieFilter.vue
+++ src/components/MovieFilter.vue
@@ -2,23 +2,30 @@
   <div id="movie-filter">
     <h2>Filter Results</h2>
     <div class="filter-group">
-      <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>
+      <check-filter v-for="time in times"
+                    category="time"
+                    v-bind:title="time">
+      </check-filter>
+    </div>
+    <h3>By genre</h3>
+    <div class="filter-group">
+      <check-filter v-for="genre in genres"
+                    category="genre"
+                    v-bind:title="genre"
+                    v-on:check-filter="checkFilter">
+      </check-filter>
     </div>
   </div>
 </template>
 
 <script>
   import genres from "../util/genres";
+  import times from "../util/times";
   import CheckFilter from "./CheckFilter.vue";
   export default {
-    methods: {
-      checkFilter(category, title, checked) {
-        this.$emit('check-filter', category, title, checked)
-      }
-    },
     data() {
       return {
-        genres
+        genres, times
       }
     },
     components: {
--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,35 +1,75 @@
 <template>
   <div id="movie-list">
-    <div v-for="movie in filteredMovies" class="movie">{{ movie.title }}</div>
+    <div v-if="filteredMovies.length">
+      <movie-item v-for="movie in filteredMovies"
+                  v-bind:movie="movie.movie"
+                  v-bind:sessions="movie.sessions"
+                  v-bind:day="day"
+                  v-bind:time="time">
+      </movie-item>
+    </div>
+    <div v-else-if="movies.length" class="no-results">
+      {{  noResults }}
+    </div>
+    <div v-else class="no-results">
+      Loading...
+    </div>
   </div>
 </template>
 <script>
   import genres from "../util/genres";
+  import times from "../util/times";
+
+  import MovieItem from './MovieItem.vue'
+
   export default {
-    data: function () {
-      return {
-        movies: [
-          {title: 'Back to the Future 1', genre: genres.COMEDY },
-          {title: 'The Matrix', genre: genres.SCIFI },
-          {title: 'The Bridges of Madison County', genre: genres.DRAMA },
-        ],
-      };
-    },
-    props: ['genre', 'time'],
+    props: ['genre', 'time', 'movies', 'day'],
     methods: {
       moviePassesGenreFilter(movie){
         if(!this.genre.length) {
           return true;
         } else {
-          return this.genre.find(genre => movie.genre === genre);
+          let movieGenres = movie.movie.Genre.split(', ');
+          let matched = true;
+          this.genre.forEach(genre => {
+            if(movieGenres.indexOf(genre) === -1) {
+              matched = false
+            }
+          });
+          return matched;
         }
-      }
+      },
+      sessionPassesTimeFilter(session) {
+        if(!this.day.isSame(this.$moment(session.time), 'day')) {
+          return false;
+        } else if (this.time.length === 0 || this.time.length === 2) {
+
+        } else if (this.time[0] === times.AFTER_6PM){
+          return this.$moment(session.time).hour() >= 18;
+        } else {
+          return this.$moment(session.time).hour() < 18;
+        }
+        return true;
+      },
     },
     computed: {
       filteredMovies(){
-        return this.movies.filter(this.moviePassesGenreFilter);
+        return this.movies
+            .filter(this.moviePassesGenreFilter)
+            .filter(movie => movie.sessions.find(this.sessionPassesTimeFilter));
       },
+      noResults() {
+        let times = this.time.join(', ');
+        let genres = this.genre.join(', ');
+        return `No results for ${times}${times.length && genres.length ? ", ": ""} ${genres}.`
+      }
     },
+    components: {
+      MovieItem,
+    },
+    created() {
+      //console.log(this.$moment)
+    }
   }
 </script>
 
